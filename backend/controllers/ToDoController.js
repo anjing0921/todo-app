616d82ea7382766f6e9c714bc675680eca5b9145
anjing0921 <duyuan0921@gmial.com>
2024-03-01 14:41:10 -0800

    add update module

--- backend/controllers/ToDoController.js
+++ backend/controllers/ToDoController.js
@@ -1,7 +1,7 @@
 const ToDoModel = require('../models/ToDoModel')
 
 module.exports.getToDo = async (req, res) => {
-  const toDo = await TodoModel.find()
+  const toDo = await ToDoModel.find()
   res.send(toDo)
 }
 
@@ -15,5 +15,15 @@ module.exports.saveToDo = async (req, res) => {
       console.log(data);
       res.send(data)
     })
+}
+
+module.exports.updateToDo = async (req, res) => {
   
+  const {_id, text} = req.body
+  
+  ToDoModel
+    .findByIdAndUpdate(_id, {text})
+    .then(()=>{ res.send("updated")
+    .catch((err)=> console.log(err))
+    })
 }
