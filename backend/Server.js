e667a51a56cfda518601fecd9e36d667c915a39e
anjing0921 <duyuan0921@gmial.com>
2024-03-01 12:50:15 -0800

    creat env and routes

--- backend/Server.js
+++ backend/Server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const routes = require('./routes/ToDoRoutes')
+
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.port || 5000
+
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(()=>console.log(`Connect to mongodb`))
+    .catch((err)=> console.log(err))
+
+app.use(routes)
 
 app.listen(PORT, ()=>console.log(`listening on: ${PORT}`))
