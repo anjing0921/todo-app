5ed3cf293790125fb7ccab9ed462e301dbaae6a9
anjing0921 <duyuan0921@gmial.com>
2024-03-01 16:32:41 -0800

    add todo.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,15 @@
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <h1>TO DO APP</h1>
+        <div className="top">
+          <input type="text" placeholder="ADD TO DO LIST"/>
+          <div className="add">Add</div>
+        </div>
+
+      </div>
+
 
     </div>
   );
---
+++ frontend/src/components/ToDo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDo = () => {
+  return (
+    <div className='todo'>
+      <div className="todo"></div>
+    </div>
+  )
+}
+
+export default ToDo
