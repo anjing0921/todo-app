ba8ee5481898723ba908c47642fe925d5cdbc481
anjing0921 <duyuan0921@gmial.com>
2024-03-01 17:31:53 -0800

    add getAllToDo api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
+import { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
+import { addToDo, getAllToDo } from "./utils/HandleApi";
 
 function App() {
+
+  const [toDo, setToDo] = useState([])
+
+  useEffect(() => {
+    getAllToDo(setToDo)
+  })
   return (
     <div className="App">
       <div className="container">
         <h1>TO DO APP</h1>
         <div className="top">
           <input type="text" placeholder="ADD TO DO LIST"/>
-          <div className="add">Add</div>
+          <div className="add" onClick={addToDo}>Add</div>
         </div>
         <div className="list">
-          <ToDo text='hi'/>
+          {toDo.map((item) =><ToDo key={item._id} text={item.text}/>)}
         </div>
       </div>
 
---
+++ frontend/src/utils/HandleApi.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const baseUrl = "http://localhost:5000"
+
+const getAllToDo = (setToDo) =>{
+  axios
+  .get(baseUrl)
+  .then(({data})=>{
+    console.log('data--->', data);
+    setToDo(data)
+  })
+}
+
+const addToDo = () =>{
+
+}
+
+export {getAllToDo, addToDo}
