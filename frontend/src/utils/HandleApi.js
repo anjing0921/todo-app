b19ba0de9d3ae27ee156030b3f624a0a0bd23fc3
anjing0921 <duyuan0921@gmial.com>
2024-03-04 12:22:22 -0800

    correct delete param

--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -38,7 +38,7 @@ const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
 const deleteToDo = (_id, setToDo) => {
   console.log(_id)
   axios
-    .delete(`${baseUrl}/delete`, { _id })
+    .delete(`${baseUrl}/delete`, { data:{_id} })
     .then((data)=>{
       console.log("call delete api")
       console.log(data)
